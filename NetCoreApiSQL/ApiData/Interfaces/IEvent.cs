30c0a72bb9360adeb582cd57e41a2a2da3044232
Haziel <mariohaziel@gmail.com>
2021-10-19 14:49:57 -0600

    WebService Creacion Evento
    Permite la insercion, eliminacion, actualizacion y obtencion de eventos almacenados en la base de datos sql mediante peticiones http

---
+++ NetCoreApiSQL/ApiData/Interfaces/IEvent.cs
@@ -0,0 +1,22 @@
+﻿using ApiModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiData.Interfaces
+{
+    /// <summary>
+    /// interfaz para entidad Evento, define get,create,delete,update
+    /// </summary>
+    public interface IEvent
+    {
+        Task<IEnumerable<Event>> GetAllEvents();
+        Task<Event> GetEvent(string event_key);
+        Task<bool> InsertEvent(Event evnt);
+        Task<bool> DeleteEvent(string event_key);
+        Task<bool> UpdateEvent(Event evnt);
+    }
+
+}
